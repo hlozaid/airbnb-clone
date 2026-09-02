91176067f52d996d16ebd7fd819484d8964024a2
Mohd Zaid <hlozaid18@gmail.com>
2026-09-03 01:32:29 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Listing = require("./models/listing.js");
+const Listing = require("./models/listings.js");
+const methodOverride = require("method-override");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,49 +22,51 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Working");
 });
 
-//Index Route
-app.get("/listing", async (req, res) => {
+// Index Route
+app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("./listings/index.ejs", { allListings });
 });
 
-//new listing route
-app.get("/listing/new", (req, res) => {
+// New listing route
+app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
-//show Route
-app.get("/listing/:id", async (req, res) => {
+// Show Route
+app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("./listings/show.ejs", { listing });
 });
 
-//create route
-app.post("/listings",async(req, res)=>{
-  let {title, description, image, price, country, location } = req.body;
+// Create route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings"); 
+});
 
-})
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing }); 
+});
 
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My new Villa",
-//     discription: " My Des",
-//     price: 1200,
-//     location: "Ighri",
-//     country: "India",
-//   });
-
-//   await sampleListing.save();
-//   console.log("Sample was saved")
-//   res.send("Ho gya bhai ho gya!")
-// });
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params; 
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`); 
+});
 
 app.listen(8080, () => {
-  console.log("Server is listing to port 8080");
+  console.log("Server is listening to port 8080");
 });
--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../models/listing.js");
+const Listing = require("../models/listings.js");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
