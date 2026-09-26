bc06a433a3d9fdd4b10bfad2bcaf8bc8fa6c0803
Mohd Zaid <hlozaid18@gmail.com>
2026-09-27 00:51:30 +0530

    update

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -30,19 +32,34 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+
 // root route
 app.get("/", (req, res) => {
   res.redirect("/listings");
 });
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next)=>{
+  res.locals.success = req.flash("success");
+  next();
+})
 
 
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
-
-
-
-
 
 // Catch-all for unmatched routes
 app.all("*splat", (req, res, next) => {
